ffc8527ba4f516ccd320ff529998049a2028659a
Alli Tzadok <allisontzadok@gmail.com>
2017-12-20 23:45:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,13 @@
 class Dog
-  def initialize(name, bark)
-    @name = name
-    @bark = "woof"
+  def name
+    @name
   end
 
   def name=(name)
     @name = name
   end
 
-  def name
-    @name
-  end
-
   def bark
-    puts "woof"
+    puts "woof!"
   end
 end
